2ffc0fef5c46a8e215e97fb3da0f981b4d1b023a
Mehmet Nizam <nizamsaltan@protonmail.com>
2022-07-02 21:30:49 +0300

    Remove firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:portfolio_app/pages/home_page.dart';
 import 'package:portfolio_app/pages/tteam_page.dart';
-import 'package:firebase_analytics/firebase_analytics.dart';
 
-FirebaseOptions get platformOptions {
-  return const FirebaseOptions(
-    apiKey: "AIzaSyDPZoBQMtPNwIRUDt6Z3R-xxfWNql1cO4E",
-    authDomain: "nizam-saltan-8d17f.firebaseapp.com",
-    projectId: "nizam-saltan-8d17f",
-    storageBucket: "nizam-saltan-8d17f.appspot.com",
-    messagingSenderId: "643540489940",
-    appId: "1:643540489940:web:c17c762224e50c25478d41",
-    measurementId: "G-7W1G15TR1S",
-  );
-}
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(options: platformOptions);
-  await FirebaseAnalytics.instance.logAppOpen();
-
+void main() {
   runApp(const MyApp());
 }
 
